4201e7f1df19439f8d44ffd7acc97b270b5df615
NikolaiPavlov <luker1998@yandex.ru>
2023-07-20 21:56:41 +0300

    add family tree v.1

---
+++ src/homework/Human.java
@@ -0,0 +1,26 @@
+package homework;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Human {
+    private String fullName;
+    private List<Human> family = new ArrayList<>();
+
+    public List<Human> getFamily() {
+        return family;
+    }
+
+    public Human(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public void appendToFamily(Human h) {
+        family.add(h);
+    }
+
+    @Override
+    public String toString() {
+        return fullName;
+    }
+}
---
+++ src/homework/OutputData.java
@@ -0,0 +1,7 @@
+package homework;
+
+public class OutputData {
+    public static void outputInfo(String data) {
+        System.out.println(data);
+    }
+}
